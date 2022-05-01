4985d08e804c6c7f75fcc1ddcddbda1622feeb11
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-05-01 09:55:04 -0700

    React walkthrough

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color: "white",
+                    background: "#f1356d",
+                    borderRadius: "0px",
+                }}>New Blog</a>
             </div>
         </nav>
      );
